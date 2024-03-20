14022a1cd4ece9f59dd341f793e4b45fc3d14fa2
Michelson Fu <mfuu0004@student.monash.edu>
2024-03-21 01:19:17 +1100

    - Removed ugie-cake from git ignore

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -13,6 +13,7 @@ class UsersController extends AppController
 
     /**
      * Initialize method
+     * Authenticates permissions access
      * 
      * @return void
      */
@@ -20,7 +21,25 @@ class UsersController extends AppController
     {
         parent::initialize();
         $this->viewBuilder()->setLayout('dashboard');
+
+        $loggedIn = false;
+        $result = $this->Authentication->getResult();
+        if ($result && $result->isValid()) {
+            $loggedIn = true;
+        }
+        $this->set('loggedIn', $loggedIn);
+        if($this->viewBuilder()->getVar('loggedIn')){
+            $user = $this->Authentication->getIdentity();
+            $user = $this->Users->get($user->user_id);
+            $this->set('role_id', $user->role_id);
+        }
+
+        // Only allow role_id = 3 (admin)
+        if ($this->viewBuilder()->getVar('role_id') !== 3) {
+            $this->redirect(['controller' => 'Pages', 'action' => 'display']);
+        }
     }
+        
 
     /**
      * Index method
@@ -28,7 +47,7 @@ class UsersController extends AppController
      * @return \Cake\Http\Response|null|void Renders view
      */
     public function index()
-    {
+    {        
         $query = $this->Users->find()
             ->contain(['Roles']);
         $users = $this->paginate($query);

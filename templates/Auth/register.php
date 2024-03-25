10323d4cefb5a63bb05668ccb2e9b1223288dd5d
vnar0004 <vnar0004@student.monash.edu>
2024-03-25 15:51:03 +1100

    fixing log in page

--- templates/Auth/register.php
+++ templates/Auth/register.php
@@ -9,49 +9,55 @@ $this->assign('title', 'Register new user');
 ?>
 <div class="container register">
     <div class="users form content">
-
         <?= $this->Form->create($user) ?>
 
-        <fieldset style="display: flex; justify-content: space-between;">
+        <fieldset>
             <legend>Register New User</legend>
 
-            <div style="width: 50%;">
-                <?= $this->Flash->render() ?>
+            <?= $this->Flash->render() ?>
 
-                <div class="input-container">
-                    <?= $this->Form->control('email', ['label' => 'Email', 'style' => 'width: 210px;']); ?>
+            <div class="input-container">
+                <div style="width: 210px;">
+                    <?= $this->Form->control('email', ['label' => 'Email']); ?>
                 </div>
+            </div>
 
-                <div class="input-container">
-                    <?= $this->Form->control('phone', ['label' => 'Phone', 'style' => 'width: 210px;']); ?>
+            <div class="input-container">
+                <div style="width: 210px;">
+                    <?= $this->Form->control('phone', ['label' => 'Phone']); ?>
                 </div>
+            </div>
 
-                <div class="input-container">
-                    <?= $this->Form->control('first_name', ['label' => 'First Name', 'style' => 'width: 210px;']); ?>
+            <div class="input-container">
+                <div style="width: 210px;">
+                    <?= $this->Form->control('first_name', ['label' => 'First Name']); ?>
                 </div>
+            </div>
 
-                <div class="input-container">
-                    <?= $this->Form->control('last_name', ['label' => 'Last Name', 'style' => 'width: 210px;']); ?>
+            <div class="input-container">
+                <div style="width: 210px;">
+                    <?= $this->Form->control('last_name', ['label' => 'Last Name']); ?>
                 </div>
+            </div>
 
-                <div class="input-container">
+            <div class="input-container">
+                <div style="width: 210px;">
                     <?= $this->Form->control('password', [
                         'value' => '',  // Ensure password is not sent back to the client side
-                        'label' => 'Password',
-                        'style' => 'width: 210px;'
-                    ]); ?>
-                </div>
-
-                <div class="input-container">
-                    <?= $this->Form->control('password_confirm', [
-                        'type' => 'password',
-                        'value' => '',  // Ensure password is not sent back to the client side
-                        'label' => 'Retype Password',
-                        'style' => 'width: 210px;'
+                        'label' => 'Password'
                     ]); ?>
                 </div>
             </div>
 
+            <div class="input-container" style="margin-bottom: 10px;"> <!-- Add margin-bottom here -->
+                <div style="width: 210px;">
+                    <?= $this->Form->control('password_confirm', [
+                        'type' => 'password',
+                        'value' => '',  // Ensure password is not sent back to the client side
+                        'label' => 'Retype Password'
+                    ]); ?>
+                </div>
+            </div>
             <div style="width: 40%;">
                 <div class="password-criteria">
                     <h4>Password Criteria:</h4>
@@ -67,8 +73,9 @@ $this->assign('title', 'Register new user');
 
         <div class="buttons">
             <?= $this->Form->button('Register') ?>
-            <?= $this->Html->link('Back to login', ['controller' => 'Auth', 'action' => 'login'], ['class' => 'button button-outline float-right']) ?>
         </div>
+
+        <?= $this->Html->link('Back to login', ['controller' => 'Auth', 'action' => 'login'], ['class' => 'button button-outline float-right']) ?>
 
         <?= $this->Form->end() ?>
     </div>

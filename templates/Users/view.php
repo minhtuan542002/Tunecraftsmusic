6e2153d1a8d7500d7a055d2ab42e693e5629e820
Michelson Fu <mfuu0004@student.monash.edu>
2024-04-09 13:45:11 +1000

    - removed aside actions sidebar styling

--- templates/Users/view.php
+++ templates/Users/view.php
@@ -8,10 +8,6 @@
 <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet" />
 
 <style>
-    .content {
-        margin: 20px;
-    }
-
     .user-details table {
         width: 100%;
         border-collapse: collapse;
@@ -31,31 +27,9 @@
         padding: 10px;
         border-left: 3px solid #3498db;
     }
-
-    .user-actions {
-        margin-top: 20px;
-    }
-
-    .side-nav {
-        padding-left: 10px;
-    }
-
-    .side-nav h4.heading {
-        margin-top: 0;
-        color: #3498db;
-    }
-
-    .btn {
-        margin-right: 10px;
-        margin-bottom: 10px;
-    }
-
-    .btn i {
-        margin-right: 5px;
-    }
 </style>
 
-<div class="content">
+<div class="mt-3">
     <h3><?= h("View User") ?></h3>
     <div class="user-details">
         <table>
@@ -92,15 +66,11 @@
         </div>
     </div>
 </div>
-
-<aside class="user-actions">
-    <div class="side-nav">
-        <h4 class="heading"><?= __('Actions') ?></h4>
-        <?= $this->Html->link('<i class="fas fa-chevron-left fa-fw"></i> Back', ['action' => 'index'], ['escape' => false, 'class' => 'btn btn-primary']) ?>
-        <?= $this->Html->link('<i class="fas fa-edit fa-fw"></i> Edit', ['action' => 'edit', $user->user_id], ['escape' => false, 'class' => 'btn btn-success']) ?>
-        <?= $this->Form->postLink('<i class="fas fa-trash fa-fw"></i> Delete', ['action' => 'delete', $user->id], ['escape' => false, 'class' => 'btn btn-danger', 'confirm' => __('Are you sure you want to delete # {0}?', $user->first_name)]) ?>
-    </div>
-</aside>
+<div class="d-flex gap-3 mt-3">
+    <?= $this->Html->link('<i class="fas fa-chevron-left fa-fw"></i> Back', ['action' => 'index'], ['escape' => false, 'class' => 'btn btn-primary']) ?>
+    <?= $this->Html->link('<i class="fas fa-edit fa-fw"></i> Edit', ['action' => 'edit', $user->user_id], ['escape' => false, 'class' => 'btn btn-success']) ?>
+    <?= $this->Form->postLink('<i class="fas fa-trash fa-fw"></i> Delete', ['action' => 'delete', $user->id], ['escape' => false, 'class' => 'btn btn-danger', 'confirm' => __('Are you sure you want to delete # {0}?', $user->first_name)]) ?>
+</div>
 
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>

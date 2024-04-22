f463f70397690a29813e5a4dc93c75612d7bfe89
Michelson Fu <mfuu0004@student.monash.edu>
2024-04-23 03:53:21 +1000

    - Updated Users Index page to use boostrap 5

--- templates/Users/index.php
+++ templates/Users/index.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>User Management</title>
-    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
-    <link href="https://cdn.datatables.net/1.13.6/css/dataTables.bootstrap4.min.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdn.datatables.net/1.13.6/css/dataTables.bootstrap5.min.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
 </head>
 <body>
@@ -15,13 +15,13 @@
      * @var iterable<\App\Model\Entity\User> $users
      */
     ?>
-    <div class="card shadow mt-4 mb-4">
+    <div class="card shadow mt-4">
         <div class="card-header py-3">
-            <h6 class="m-0 font-weight-bold text-primary">Users</h6>
+            <h2 class="m-0 font-weight-bold text-primary">Manage <em>Users</em></h2>
         </div>
         <div class="card-body">
             <div class="table-responsive">
-                <table class="table table-bordered" id="dataTable">
+                <table class="table table-striped" id="dataTable">
                     <thead>
                         <tr>
                             <th>First Name</th>
@@ -52,30 +52,25 @@
 
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
-    <script src="https://cdn.datatables.net/1.13.6/js/dataTables.bootstrap4.min.js"></script>
+    <script src="https://cdn.datatables.net/1.13.6/js/dataTables.bootstrap5.min.js"></script>
     <script>
         $(document).ready(function() {
             $('#dataTable').DataTable({
                 "paging": true,
                 "ordering": true,
                 "searching": true,
-                "columnDefs": [
-                    {
-                        "targets": [3], // User ID as the fourth column (0-based index)
-                        "visible": true,
-                        "searchable": true
-                    },
-                    {
-                        "targets": [4],
-                        "orderable": false
-                    }
-                ],
+                "lengthMenu": [10, 25, 50, 100], // Show entries options
                 "language": {
-                    "emptyTable": "No data available in table",
-                    "search": '<i class="fas fa-search" aria-hidden="true"></i>',
-                    "searchPlaceholder": "Search...",
+                    "lengthMenu": "Show _MENU_ entries", // Customize show entries label
+                    "search": '<i class="fas fa-search" aria-hidden="true"></i>', // Custom search icon
+                    "searchPlaceholder": "Search...", // Placeholder text for search input
                 },
-                "dom": '<"top"lf>rt<"bottom"ip><"clear">'
+                "columnDefs": [
+                    { "targets": 4, "sortable": false, "searchable": false } // Disable sorting and searching for the Actions column (0-based index)
+                ],
+                "dom": '<"row align-items-center mb-3"<"col-md-6"l><"col-md-6"f>>' +
+                       '<"table-responsive"t>' +
+                       '<"row align-items-center mt-3"<"col-md-6"i><"col-md-6"p>>', // Adjust the DOM layout for search and show entries on the same row
             });
         });
     </script>

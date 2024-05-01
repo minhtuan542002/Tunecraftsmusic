d784a279b1e84c1afa7ca4f230a5c3b2de3834c1
Michelson Fu <mfuu0004@student.monash.edu>
2024-05-01 17:39:29 +1000

    - updated bookings styling to match other index pages

--- templates/Bookings/index.php
+++ templates/Bookings/index.php
@@ -5,87 +5,79 @@
  */
 $this->layout = 'dashboard';
 ?>
-<link href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css" rel="stylesheet" />
-<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet" />
+<link href="https://cdn.datatables.net/1.13.6/css/dataTables.bootstrap5.min.css" rel="stylesheet">
+<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
 
-<section class="page-section clearfix">
-    <div class="bookings index mt-3">
-        <h3><?= __('All Bookings') ?></h3>
-        <div class="table-responsive user-table-container">
-            <table class="table dataTable" id="dataTable">
+<div class="card shadow mt-4">
+    <div class="card-header py-3">
+        <h2 class="m-0 font-weight-bold text-primary">Manage <em>Bookings</em></h2>
+    </div>
+    <div class="card-body">
+        <div class="table-responsive">
+            <table class="table table-striped" id="dataTable">
                 <thead>
                     <tr>
-                        <th><?= h('Customer Name') ?></th>
-                        <th><?= h('Weekly Date & Time') ?></th>
-                        <th><?= h('Remaining Lessons') ?></th>
-                        <th><?= h('Upcoming Lesson') ?></th>
-                        <th><?= h('Each Lesson Duration') ?></th>
-                        <th><?= h('Is Paid for') ?></th>
-                        <th><?= h('Booking ID') ?></th>
+                        <th>Customer Name</th>
+                        <th>Weekly Date & Time</th>
+                        <th>Remaining Lessons</th>
+                        <th>Upcoming Lesson</th>
+                        <th>Each Lesson Duration</th>
+                        <th>Is Paid for</th>
+                        <th>Booking ID</th>
                         <th><?= __('Actions') ?></th>
                     </tr>
                 </thead>
                 <tbody>
                     <?php foreach ($bookings as $booking): ?>
-                    <tr>
-                        <td><?= $booking->student->user->first_name . " " . $booking->student->user->last_name ?></td>
-                        <td><?= $booking->booking_datetime->format('l H:i') ?></td>
-                        <td><?= $booking->remain_count ?></td>
-                        <td><?= $booking->upcoming != null ? $booking->upcoming->lesson_start_time->format('d/m/Y  H:i') : 'None' ?></td>
-                        <td><?= $booking->package->lesson_duration_minutes . " mins" ?></td>
-                        <td><?= $this->Form->postLink(__($booking->is_paid? "Yes":"No" ), ['action' => 'togglePaid', $booking->booking_id], 
-                            ['class' => __('btn '. ($booking->is_paid?'btn-success':'btn-outline-success') .' btn-sm paid-button'),
-                                'confirm' => __('Are you sure you want confirm the payment status as '. ($booking->is_paid? "UNPAID":"PAID" ))]) ?></td>
-                        <td><?= $booking->booking_id ?></td>
-                        <td class="d-flex gap-2">
-                            <?= $this->Html->link('<i class="fas fa-eye fa-fw"></i> View', ['action' => 'view', $booking->booking_id], 
-                                ['escape' => false, 'class' => 'btn btn-primary btn-sm']) ?>
-                            <?= $this->Html->link('<i class="fas fa-edit fa-fw"></i> Edit', ['action' => 'edit_admin', $booking->booking_id], 
-                                ['escape' => false, 'class' => 'btn btn-success btn-sm']) ?>
-                            <?= $this->Form->postLink('<i class="fas fa-trash fa-fw"></i> Delete', 
-                                ['action' => 'delete', $booking->booking_id], 
-                                ['escape' => false, 'class' => 'btn btn-danger btn-sm', 
-                                    'confirm' => __('Are you sure you want to cancel booking # {0}?', $booking->booking_id)]) ?>
-                        </td>
-                    </tr>
+                        <tr>
+                            <td><?= $booking->student->user->first_name . " " . $booking->student->user->last_name ?></td>
+                            <td><?= $booking->booking_datetime->format('l H:i') ?></td>
+                            <td><?= $booking->remain_count ?></td>
+                            <td><?= $booking->upcoming != null ? $booking->upcoming->lesson_start_time->format('d/m/Y  H:i') : 'None' ?></td>
+                            <td><?= $booking->package->lesson_duration_minutes . " mins" ?></td>
+                            <td><?= $this->Form->postLink(__($booking->is_paid? "Yes":"No" ), ['action' => 'togglePaid', $booking->booking_id], 
+                                    ['class' => __('btn '. ($booking->is_paid?'btn-success':'btn-outline-success') .' btn-sm paid-button'),
+                                        'confirm' => __('Are you sure you want confirm the payment status as '. ($booking->is_paid? "UNPAID":"PAID" ))]) ?></td>
+                            <td><?= $booking->booking_id ?></td>
+                            <td class="d-flex gap-2">
+                                <?= $this->Html->link('<i class="fas fa-eye fa-fw"></i> View', ['action' => 'view', $booking->booking_id], 
+                                    ['escape' => false, 'class' => 'btn btn-primary btn-sm']) ?>
+                                <?= $this->Html->link('<i class="fas fa-edit fa-fw"></i> Edit', ['action' => 'edit_admin', $booking->booking_id], 
+                                    ['escape' => false, 'class' => 'btn btn-success btn-sm']) ?>
+                                <?= $this->Form->postLink('<i class="fas fa-trash fa-fw"></i> Delete', 
+                                    ['action' => 'delete', $booking->booking_id], 
+                                    ['escape' => false, 'class' => 'btn btn-danger btn-sm', 
+                                        'confirm' => __('Are you sure you want to cancel booking # {0}?', $booking->booking_id)]) ?>
+                            </td>
+                        </tr>
                     <?php endforeach; ?>
                 </tbody>
             </table>
         </div>
     </div>
-</section>
+</div>
 
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
 <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.13.6/js/dataTables.bootstrap5.min.js"></script>
 <script>
-    // var mut = new MutationObserver(function(mutations, mut){
-    //     $(".paid-button:contains('Yes')").addClass('btn-success');
-    //     $(".paid-button:contains('Yes')").removeClass('btn-outline-success');
-    //     $(".paid-button:contains('No')").addClass('btn-outline-success');
-    //     $(".paid-button:contains('No')").removeClass('btn-success');
-    //     console.log()
-    // });
-    // mut.observe(document.querySelector(".paid-button"),{
-    // 'text': true
-    // });
-
     $(document).ready(function() {
         $('#dataTable').DataTable({
             "paging": true,
             "ordering": true,
             "searching": true,
-            "columnDefs": [
-                {
-                    "targets": [7],
-                    "orderable": false
-                }
-            ],
+            "lengthMenu": [10, 25, 50, 100], // Show entries options
             "language": {
-                "emptyTable": "No data available in table",
-                "search": '<i class="fas fa-search"></i>',
-                "searchPlaceholder": "Search...",
+                "lengthMenu": "Show _MENU_ entries", // Customize show entries label
+                "search": '<i class="fas fa-search" aria-hidden="true"></i>', // Custom search icon
+                "searchPlaceholder": "Search...", // Placeholder text for search input
             },
-            "dom": '<"top"lf>rt<"bottom"ip><"clear">'
+            "columnDefs": [
+                { "targets": 7, "sortable": false, "searchable": false } // Disable sorting and searching for the Actions column (0-based index)
+            ],
+            "dom": '<"row align-items-center mb-3"<"col-md-6"l><"col-md-6"f>>' +
+                   '<"table-responsive"t>' +
+                   '<"row align-items-center mt-3"<"col-md-6"i><"col-md-6"p>>', // Adjust the DOM layout for search and show entries on the same row
         });
     });
 </script>

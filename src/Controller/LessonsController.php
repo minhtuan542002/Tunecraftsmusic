fd657d364657d3b4b19e9947a9752af90da808b2
Michelson Fu <mfuu0004@student.monash.edu>
2024-06-11 18:04:58 +1000

    - updated success flash reschedule notification email

--- src/Controller/LessonsController.php
+++ src/Controller/LessonsController.php
@@ -264,7 +264,7 @@ class LessonsController extends AppController
                         'lesson_time' => $reschedule_notification->lesson_start_time->format('H:i')
                     ]);
 
-                    $this->Flash->success(__($reschedule_notification->booking->student->user->email));
+                    $this->Flash->success(__('Notification email sent to: ' && $reschedule_notification->booking->student->user->email));
 
                     return $this->redirect(['action'=>'edit_admin', $id]);
                     //return $this->redirect(['controller' => 'bookings', 'action' => 'view', $lesson->booking_id]);

285347002b4cfc114779003c3dae9ebd6d501173
Michelson Fu <mfuu0004@student.monash.edu>
2024-06-11 18:01:27 +1000

    - debug message

--- src/Controller/LessonsController.php
+++ src/Controller/LessonsController.php
@@ -264,6 +264,8 @@ class LessonsController extends AppController
                         'lesson_time' => $reschedule_notification->lesson_start_time->format('H:i')
                     ]);
 
+                    $this->Flash->success(__($reschedule_notification->booking->student->user->email));
+
                     return $this->redirect(['action'=>'edit_admin', $id]);
                     //return $this->redirect(['controller' => 'bookings', 'action' => 'view', $lesson->booking_id]);
                 }
